fb41671fdd4178d373fcb4e2a3864de23b356f51
Scott Lawrence <bytbox@gmail.com>
2011-11-21 15:32:46 -0500

    First steps toward parsing

--- Data/HEPEVT.hs
+++ Data/HEPEVT.hs
@@ -7,3 +7,14 @@ the ASCII HEPEVT file format.
 module Data.HEPEVT (
 
 ) where
+
+type Event = ([Double], [[Double]])
+
+parseEventFile :: String -> IO [Event]
+parseEventFile fname = do
+  readFile fname >>= return . parseEvents fname
+
+parseEvents :: String -> String -> [Event]
+parseEvents fname dat =
+  let ls = lines dat in
+    []
