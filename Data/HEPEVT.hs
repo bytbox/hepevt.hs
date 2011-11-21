de3f8e018f2a78f71eb65ed8189b6651e14360ef
Scott Lawrence <bytbox@gmail.com>
2011-11-21 17:13:16 -0500

    Some parsing progress (need to use the state monad)

--- Data/HEPEVT.hs
+++ Data/HEPEVT.hs
@@ -11,13 +11,23 @@ module Data.HEPEVT (
   parseEvents,
 ) where
 
+import qualified Data.ByteString.Char8 as S
+
 type Event = ([Double], [[Double]])
 
 parseEventFile :: String -> IO [Event]
 parseEventFile fname = do
-  readFile fname >>= return . parseEvents fname
+  S.readFile fname >>= return . parseEvents fname
 
-parseEvents :: String -> String -> [Event]
+parseEvents :: String -> S.ByteString -> [Event]
 parseEvents fname dat =
-  let ls = lines dat in
-    []
+  let ls = S.split '\n' dat in 
+    foldl procLine [] ls
+  where
+    procLine events line =
+      let ws = S.split ' ' line in
+        if length ws < 1 then events else
+          let h = head ws in
+            process (S.unpack h) events (tail ws)
+    process "P" events line = ([], []) : events
+    process _ events _ = events
