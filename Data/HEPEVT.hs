173bb7ffda60564c93de89ac9c7d0becbf00bc7c
Scott Lawrence <bytbox@gmail.com>
2012-01-21 22:01:22 -0500

    Fixing typo

--- Data/HEPEVT.hs
+++ Data/HEPEVT.hs
@@ -1,6 +1,6 @@
 {-|
 
-This modules provides a pure haskell implementation of a reader and writer for
+This module provides a pure haskell implementation of a reader and writer for
 the ASCII HEPEVT file format.
 
 -}
